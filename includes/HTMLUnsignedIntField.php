bae06e98bb55b5fd363817cdf6f2f91c665d41ab
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2023-01-09 11:18:05 +0200

    HTMLUnsignedIntField: make sure not to pass null to trim()

--- includes/HTMLUnsignedIntField.php
+++ includes/HTMLUnsignedIntField.php
@@ -4,18 +4,18 @@
  * A field that must contain a number
  */
 class HTMLUnsignedIntField extends HTMLIntField {
-	function validate( $value, $alldata ) {
+	/** @inheritDoc */
+	public function validate( $value, $alldata ) {
 		$p = parent::validate( $value, $alldata );
 
 		if ( $p !== true ) {
 			return $p;
 		}
 
-		if ( !preg_match( '/^([1-9][0-9]*)?$/', trim( $value ) ) ) {
+		if ( !preg_match( '/^([1-9][0-9]*)?$/', trim( $value ?? '' ) ) ) {
 			return $this->msg( 'htmlform-unsigned-int-invalid' )->parseAsBlock();
 		}
 
 		return true;
 	}
 }
-
